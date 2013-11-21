b0781407a2a339f8f8ce44dd1289c17bfd2e85b6
José Pablo <josepablomatamoros@gmail.com>
2013-11-21 02:42:03 -0600

    Cambios en el include

--- admin/data_insertion/add_formato_form.php
+++ admin/data_insertion/add_formato_form.php
@@ -87,14 +87,9 @@
 
     </div><!--/.container-->
 
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/offcanvas.js"></script>
+	<?php 
+	include ("../html/script.php");
+	?>
     <script>
 	$(document).ready(function(){
 		$(".add_ok").delay(4000).fadeOut();
