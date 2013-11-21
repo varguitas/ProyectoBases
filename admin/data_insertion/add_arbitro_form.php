5ee0a15e32213b238d48588670e2cd42a70f1d67
José Pablo <josepablomatamoros@gmail.com>
2013-11-21 02:01:27 -0600

    Cambios en el include

--- admin/data_insertion/add_arbitro_form.php
+++ admin/data_insertion/add_arbitro_form.php
@@ -141,16 +141,9 @@
       </footer>
 
     </div><!--/.container-->
-
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
- 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-  	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/offcanvas.js"></script>
+	<?php 
+	include ("../html/script.php");
+	?>
     <script src="../js/index.js"></script>
         <script>
 	$(document).ready(function(){
---
+++ admin/html/script.php
@@ -0,0 +1,9 @@
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+ 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/offcanvas.js"></script>
+    
+    
