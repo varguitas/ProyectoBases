6ec9b58a448b5bb0f9788b1c1736eb14c329ffb9
José Pablo <josepablomatamoros@gmail.com>
2013-11-21 02:47:18 -0600

    Cambios en l include

--- admin/data_insertion/add_jugador_form.php
+++ admin/data_insertion/add_jugador_form.php
@@ -156,14 +156,9 @@
 
     </div><!--/.container-->
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
- 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-  	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/offcanvas.js"></script>
-    <script src="../js/index.js"></script>
+    <?php 
+	include ("../html/script.php");
+	?>
     <script>
 	$(document).ready(function(){
 		$(".add_ok").delay(4000).fadeOut();
